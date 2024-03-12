fb9948334ab1c097e34ed00a9636bac5ac7eccbf
which <whichbei@gmail.com>
2024-03-12 18:30:46 +0800

    update 优化etcd注册中心

--- freefish-rpc-core/src/main/java/com/which/rpc/registry/EtcdRegistry.java
+++ freefish-rpc-core/src/main/java/com/which/rpc/registry/EtcdRegistry.java
@@ -1,15 +1,22 @@
 package com.which.rpc.registry;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.collection.ConcurrentHashSet;
+import cn.hutool.cron.CronUtil;
+import cn.hutool.cron.task.Task;
 import cn.hutool.json.JSONUtil;
 import com.which.rpc.config.RegistryConfig;
 import com.which.rpc.model.ServiceMetaInfo;
 import io.etcd.jetcd.*;
 import io.etcd.jetcd.options.GetOption;
 import io.etcd.jetcd.options.PutOption;
+import io.etcd.jetcd.watch.WatchEvent;
 
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -26,6 +33,21 @@ public class EtcdRegistry implements Registry {
     private Client client;
     private KV kvClient;
 
+    /**
+     * 本机注册的节点 key 集合（用于维护续期）
+     */
+    private final Set<String> localRegisterNodeKeySet = new HashSet<>();
+
+    /**
+     * 注册中心服务缓存
+     */
+    private final RegistryServiceCache registryServiceCache = new RegistryServiceCache();
+
+    /**
+     * 正在监听的 key 集合
+     */
+    private final Set<String> watchingKeySet = new ConcurrentHashSet<>();
+
     @Override
     public void init(RegistryConfig registryConfig) {
         client = Client.builder()
@@ -33,6 +55,7 @@ public class EtcdRegistry implements Registry {
                 .connectTimeout(Duration.ofMillis(registryConfig.getTimeout()))
                 .build();
         kvClient = client.getKVClient();
+        heartBeat();
     }
 
     @Override
@@ -40,8 +63,8 @@ public class EtcdRegistry implements Registry {
         // 创建 Lease 和 KV 客户端
         Lease leaseClient = client.getLeaseClient();
 
-        // todo 创建一个 30 秒的租约，目前 300 秒
-        long leaseId = leaseClient.grant(300).get().getID();
+        // 创建一个 30 秒的租约
+        long leaseId = leaseClient.grant(30).get().getID();
 
         // 设置要存储的键值对
         String registerKey = ETCD_ROOT_PATH + serviceMetaInfo.getServiceNodeKey();
@@ -51,16 +74,31 @@ public class EtcdRegistry implements Registry {
         // 将 键值对 与 租约 关联起来，并设置过期时间
         PutOption putOption = PutOption.builder().withLeaseId(leaseId).build();
         kvClient.put(key, value, putOption).get();
+
+        // 添加节点信息到本地缓存
+        localRegisterNodeKeySet.add(registerKey);
     }
 
     @Override
     public void unRegister(ServiceMetaInfo serviceMetaInfo) {
-        String registerKey = ETCD_ROOT_PATH + serviceMetaInfo.getServiceNodeKey();
-        kvClient.delete(ByteSequence.from(registerKey, StandardCharsets.UTF_8)).join();
+        String key = ETCD_ROOT_PATH + serviceMetaInfo.getServiceNodeKey();
+        try {
+            kvClient.delete(ByteSequence.from(key, StandardCharsets.UTF_8)).join();
+        } catch (Exception e) {
+            throw new RuntimeException(key + "节点下线失败");
+        }
+        // 也要从本地缓存移除
+        localRegisterNodeKeySet.remove(key);
     }
 
     @Override
     public List<ServiceMetaInfo> serviceDiscovery(String serviceKey) {
+        // 优先从缓存获取服务
+        List<ServiceMetaInfo> cachedServiceMetaInfoList = registryServiceCache.readCache();
+        if (cachedServiceMetaInfoList != null) {
+            return cachedServiceMetaInfoList;
+        }
+
         // 前缀搜索，结尾一定要加 '/'
         String searchPrefix = ETCD_ROOT_PATH + serviceKey + "/";
 
@@ -73,12 +111,18 @@ public class EtcdRegistry implements Registry {
                     .get()
                     .getKvs();
             // 解析服务信息
-            return keyValues.stream()
+            List<ServiceMetaInfo> serviceMetaInfoList = keyValues.stream()
                     .map(keyValue -> {
+                        String key = keyValue.getKey().toString(StandardCharsets.UTF_8);
+                        // 监听 key 的变化
+                        watch(key);
                         String value = keyValue.getValue().toString(StandardCharsets.UTF_8);
                         return JSONUtil.toBean(value, ServiceMetaInfo.class);
                     })
                     .collect(Collectors.toList());
+            // 写入服务缓存
+            registryServiceCache.writeCache(serviceMetaInfoList);
+            return serviceMetaInfoList;
         } catch (Exception e) {
             throw new RuntimeException("获取服务列表失败", e);
         }
@@ -87,6 +131,14 @@ public class EtcdRegistry implements Registry {
     @Override
     public void destroy() {
         System.out.println("当前节点下线");
+        // 遍历本节点所有的 key
+        for (String key : localRegisterNodeKeySet) {
+            try {
+                kvClient.delete(ByteSequence.from(key, StandardCharsets.UTF_8)).join();
+            } catch (Exception e) {
+                throw new RuntimeException(key + "节点下线失败");
+            }
+        }
         // 释放资源
         if (kvClient != null) {
             kvClient.close();
@@ -95,4 +147,68 @@ public class EtcdRegistry implements Registry {
             client.close();
         }
     }
+
+    @Override
+    public void heartBeat() {
+        // 10 秒续签一次
+        CronUtil.schedule("*/10 * * * * *", new Task() {
+            @Override
+            public void execute() {
+                // 遍历本节点所有的 key
+                for (String key : localRegisterNodeKeySet) {
+                    try {
+                        List<KeyValue> keyValues = kvClient.get(ByteSequence.from(key, StandardCharsets.UTF_8))
+                                .get()
+                                .getKvs();
+                        // 该节点已过期（需要重启节点才能重新注册）
+                        if (CollUtil.isEmpty(keyValues)) {
+                            continue;
+                        }
+                        // 节点未过期，重新注册（相当于续签）
+                        KeyValue keyValue = keyValues.get(0);
+                        String value = keyValue.getValue().toString(StandardCharsets.UTF_8);
+                        ServiceMetaInfo serviceMetaInfo = JSONUtil.toBean(value, ServiceMetaInfo.class);
+                        register(serviceMetaInfo);
+                    } catch (Exception e) {
+                        throw new RuntimeException(key + "续签失败", e);
+                    }
+                }
+            }
+        });
+        // 支持秒级别定时任务
+        CronUtil.setMatchSecond(true);
+        CronUtil.start();
+        System.out.println("续签进行中...");
+    }
+
+    /**
+     * 监听（消费端）
+     *
+     * @param serviceNodeKey
+     */
+    @Override
+    public void watch(String serviceNodeKey) {
+        Watch watchClient = client.getWatchClient();
+        // 之前未被监听，开启监听
+        boolean newWatch = watchingKeySet.add(serviceNodeKey);
+        if (!newWatch) {
+            // 重复监听
+            return;
+        }
+        watchClient.watch(ByteSequence.from(serviceNodeKey, StandardCharsets.UTF_8), response -> {
+            for (WatchEvent event : response.getEvents()) {
+                switch (event.getEventType()) {
+                    // key 删除时触发
+                    case DELETE:
+                        // 清理注册服务缓存
+                        registryServiceCache.clearCache();
+                        break;
+                    case PUT:
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+
 }
---
+++ freefish-rpc-core/src/main/java/com/which/rpc/registry/RegistryServiceCache.java
@@ -0,0 +1,45 @@
+package com.which.rpc.registry;
+
+import com.which.rpc.model.ServiceMetaInfo;
+
+import java.util.List;
+
+/**
+ * 注册中心服务本地缓存
+ *
+ * @author which
+ */
+public class RegistryServiceCache {
+
+    /**
+     * 服务缓存
+     */
+    List<ServiceMetaInfo> serviceCache;
+
+    /**
+     * 写缓存
+     *
+     * @param newServiceCache
+     * @return
+     */
+    void writeCache(List<ServiceMetaInfo> newServiceCache) {
+        this.serviceCache = newServiceCache;
+    }
+
+    /**
+     * 读缓存
+     *
+     * @return
+     */
+    List<ServiceMetaInfo> readCache() {
+        return this.serviceCache;
+    }
+
+    /**
+     * 清空缓存
+     */
+    void clearCache() {
+        this.serviceCache = null;
+    }
+
+}
